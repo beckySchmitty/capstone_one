ff39b867e6ae06df96c2a611c4fc132e69d275a8
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2020-12-13 17:48:41 -0500

    UI tweaks

--- static/app.js
+++ static/app.js
@@ -4,10 +4,14 @@ $(function() {
 
 
 // ALERT FLASHER
-$(document).ready(function() {
-  setTimeout(function() {
-      $('.alert').fadeOut('slow');
-  }, 2000);
+
+
+  $( "#email-icon" ).mouseenter(function() {
+    $( this ).removeClass( "fas fa-envelope fa-5x" ).addClass( "fas fa-envelope-open fa-5x btn-r-2");
+  })
+  .mouseleave(function() {
+    $( this ).removeClass( "fas fa-envelope-open fa-5x btn-r-2" ).addClass( "fas fa-envelope fa-5x");
+  });
 
 
 
