b09d131309dadf864b42df0c36a867bcda2a7406
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2020-12-14 22:41:51 -0500

    added email_validator for forms

--- forms.py
+++ forms.py
@@ -1,17 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, FloatField, BooleanField
-from wtforms.validators import DataRequired, Length, InputRequired, Optional
+from wtforms.validators import DataRequired, Length, InputRequired, Optional, Email
 
 states = ['al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'dc', 'de', 'fl', 'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks', 'ky', 'la', 'me', 'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 'nj', 'nm', 'ny', 'nc', 'nd', 'oh', 'ok', 'or', 'pa', 'ri', 'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 'wy', 'as', 'fm', 'mh', 'mp', 'pr', 'pw', 'vi' ]
-
-# /add correct email validators
 
 class userSignUpForm(FlaskForm):
     """Form to sign up user"""
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6), DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
     address_line1 = StringField('Address Line 1', validators=[DataRequired()])
     address_line2 = StringField('Address Line 2', validators=[Optional()])
     state_name = SelectField('State',validators=[DataRequired()], choices=[(state, state.upper()) for state in states])
@@ -26,7 +24,7 @@ class userLoginForm(FlaskForm):
 class editUserForm(FlaskForm):
     """form to edit user"""
 
-    email = StringField('E-mail', validators=[Optional()])
+    email = StringField('E-mail', validators=[Optional(), Email()])
     username = StringField('Username', validators=[Optional()])
 
 
